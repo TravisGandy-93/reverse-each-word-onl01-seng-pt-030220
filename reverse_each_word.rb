497480fb65e9f585e85dd5817cc56e2d73e1a94a
Travis Gandy <travis_gandy@yahoo.com>
2020-03-15 05:50:00 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(sentence)
+  words = sentence.split(/ /)
+  flipped_words = []
+  words.each do |flip|
+   flipped_words << flip.reverse
+ end
+ flipped_words.join(" ")
+ end
+ 
+def reverse_each_word(sentence)
+  words = sentence.split(/ /)
+  flipped_words = []
+  words.collect do |flip|
+   flipped_words << flip.reverse
+ end
+ flipped_words.join(" ")
+ end
